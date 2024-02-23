c9e879f34cd5e05a0bc08e6807240e730c283318
viperofficailll <sharadhapokharel777@gmail.com>
2024-02-23 12:40:04 +0545

    sabbai hai

---
+++ admin.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin</title>
+</head>
+<body><h2></h2>
+enter username and password for login
+<div class="login-container">
+        <form id="loginForm" action="admin.php" method="post">
+           
+            <input type="text" id="username" name="username" placeholder="enter username" required>
+
+            
+            <input type="password" id="password" name="passwd"placeholder="enter password" required>
+
+            
+            <button type="submit" name="submit" value="submit">Login</button>
+        </form>
+
+    </div>
+    <?php
+    include 'db_conn.php';
+    
+if (isset($_POST['submit'])) {
+    $name = $_POST['username'];
+    $passwd = $_POST['passwd'];
+    $query = "SELECT * from `admindata` WHERE `username` = '$name' AND `PASSWORD` = '$passwd'";
+    $total = mysqli_num_rows($data);
+    echo("hello");
+    if ($total == 1 && $data != null) {
+        // Valid credentials
+       echo("welcome admin");
+        
+        
+    } else {
+        echo "Incorrect email address or password";
+    }
+}
+
+
+?>
+     
+     
+    
+</body>
+</html>
